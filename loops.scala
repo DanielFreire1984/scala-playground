7ab3fd4baf9adf9f59918411d4db70fc3f948d3c
Hussama Ismail <hussamaismail@gmail.com>
2016-08-24 09:49:12 +0200

    including a example with yield for loops

--- loops.scala
+++ loops.scala
@@ -3,16 +3,17 @@
  * @author: Hussama Ismail
  * @date: 08/23/2016
  *
- * NOTE: This examples use 'for' loop, which include:
- * 
+ * NOTE: This examples use 'for' loop, which includes:
  * 1) simple range
  * 2) two ranges
  * 3) binary counting
  * 4) numeric list 
  * 5) filtering a list using filters
+ * 6) yielding a new list using filters
  */
 
 object Loops {
+
 	def main (args: Array[String]){
 		
 		var a = 0;
@@ -39,7 +40,7 @@ object Loops {
 		}
 		
 		println("\n5) Printing elements inside a list ");
-		var list = List(1, 2, 3, 4, 5);
+		var list = List(1, 2, 3, 4, 5, 6);
 		for (a <- list){
 			println(a);
 		}
@@ -47,7 +48,14 @@ object Loops {
 		println("\n6) Printing even elements inside a list (using filters)");				
 		for (a <- list; if (a % 2 == 0)){
 			println(a);
-		}		 
+		}
+
+		println("\n7) Yielding a new list according filters ");				
+		var new_list = for { a <- list if (a != 3); if a < 5} yield a; 		
+		for (e <- new_list){
+		   println(e);
+		}
 		
 	}
+	
 }
