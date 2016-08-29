95561107d596ffb5390787d4932fb8d036b444a3
Hussama Ismail <hussamaismail@gmail.com>
2016-08-29 23:05:36 +0200

    first file with classes

---
+++ classes.scala
@@ -0,0 +1,18 @@
+
+/*
+class Person (name : String) {
+  def displayName() : Unit = { 
+    println("OK");
+  }   
+} 
+*/
+
+object Person {
+  def main (args: Array[String]){ 
+  //  var p = new Person("Hussama");
+  //  p.displayName();
+    println("FIM");
+  
+  }
+
+}
